926c55dc7298719632da0e5b837044f7cb5ea5c0
Tima <tima52rus@gmail.com>
2022-05-09 15:58:23 +0300

    add react-router-dom, and create routes

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {HashRouter, Routes, Route} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App/>
+        <HashRouter>
+            <Routes>
+                <Route path={'/'} element={<App/>}/>
+        {/*<App/>*/}
+            </Routes>
+        </HashRouter>
     </React.StrictMode>
 );
 
